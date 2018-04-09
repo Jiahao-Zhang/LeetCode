f149e98e852f31b43270e8c7975af42efeff3795
Jiahao <JiahaoZhang2018@outlook.com>
2018-04-09 10:31:52 +0800

    Array

---
+++ array/ToeplitzMatrix.java
@@ -0,0 +1,16 @@
+package pers.jiahao.leetcode.array;
+
+public class ToeplitzMatrix {
+	 public boolean isToeplitzMatrix(int[][] matrix) {
+	        boolean result = true;
+	        for(int i = 0;i<matrix.length-1;i++){
+	            for(int j = 0;j<matrix[0].length-1;j++){
+	                if(matrix[i][j]!=matrix[i+1][j+1]){
+	                    result = false;
+	                    return result;
+	                }
+	            }
+	        }
+	        return result;
+	    }
+}
