d2cbed985fcf6c2cb596617e4e617fe7a68ff4eb
Jiahao <JiahaoZhang2018@outlook.com>
2018-06-02 23:24:39 +0800

    Math

---
+++ math/NO537ComplexNumberMultiplication.java
@@ -0,0 +1,9 @@
+package pers.jiahao.leetcode.math;
+/** 
+* @author Jiahao Zhang
+* @version ：Jun 2, 2018 11:20:55 PM 
+* 类说明 
+*/
+public class NO537ComplexNumberMultiplication {
+
+}
---
+++ math/NO728SelfDividingNumbers.java
@@ -0,0 +1,9 @@
+package pers.jiahao.leetcode.math;
+/** 
+* @author Jiahao Zhang
+* @version ：Jun 2, 2018 11:18:44 PM 
+* 类说明 
+*/
+public class NO728SelfDividingNumbers {
+
+}
