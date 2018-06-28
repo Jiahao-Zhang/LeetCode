7355094d498d2d46147ebece883d7f1695b12b05
Jiahao <JiahaoZhang2018@outlook.com>
2018-06-28 22:21:59 +0800

    DP

---
+++ dp/NO64MinimumPathSum.java
@@ -0,0 +1,9 @@
+package pers.jiahao.leetcode.dp;
+/** 
+* @author Jiahao Zhang
+* @version ：Jun 28, 2018 10:20:55 PM 
+* 类说明 
+*/
+public class NO64MinimumPathSum {
+
+}
--- dp/NO746MinCostClimbingStairs.java
+++ dp/NO746MinCostClimbingStairs.java
@@ -1,9 +1,15 @@
 package pers.jiahao.leetcode.dp;
-/** 
-* @author Jiahao Zhang
-* @version ：Jun 22, 2018 10:30:06 PM 
-* 类说明 
-*/
+
 public class NO746MinCostClimbingStairs {
+	public int minCostClimbingStairs(int[] cost) {
+        int result = 0;
+        int[] dp = new int[cost.length];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for(int i = 2;i<cost.length;i++){
+            dp[i] = cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[cost.length-1],dp[cost.length-2]);
+    }
 
 }
