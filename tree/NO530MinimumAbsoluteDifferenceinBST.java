72237d276c945ff789e5d485d22c2b7c5610d2dd
Jiahao <JiahaoZhang2018@outlook.com>
2018-07-04 23:08:08 +0800

    Tree

---
+++ tree/NO530MinimumAbsoluteDifferenceinBST.java
@@ -0,0 +1,9 @@
+package pers.jiahao.leetcode.tree;
+/** 
+* @author Jiahao Zhang
+* @version ：Jul 4, 2018 11:07:08 PM 
+* 类说明 
+*/
+public class NO530MinimumAbsoluteDifferenceinBST {
+
+}
--- tree/NO783MinimumDistanceBetweenBSTNodes.java
+++ tree/NO783MinimumDistanceBetweenBSTNodes.java
@@ -1,9 +1,27 @@
 package pers.jiahao.leetcode.tree;
-/** 
-* @author Jiahao Zhang
-* @version ：Jul 3, 2018 11:08:55 PM 
-* 类说明 
-*/
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NO783MinimumDistanceBetweenBSTNodes {
+	public int minDiffInBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        dfs(root,list);
+        int min = Integer.MAX_VALUE;
+        for(int i = 1;i<list.size();i++){
+            min = Math.min(min,list.get(i)-list.get(i-1));
+        }
+        return min;
+    }
+    public void dfs(TreeNode root,List<Integer> list){
+        if(root==null){
+            return;
+        }
+        
+        dfs(root.left,list);
+        list.add(root.val);
+        dfs(root.right,list);
+        
+    }
 
 }
