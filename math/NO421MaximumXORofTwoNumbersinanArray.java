77c1270632acffe61918d9c92de2d845ff10fadf
Jiahao <JiahaoZhang2018@outlook.com>
2018-07-16 23:27:12 +0800

    Math

--- math/NO421MaximumXORofTwoNumbersinanArray.java
+++ math/NO421MaximumXORofTwoNumbersinanArray.java
@@ -1,5 +1,17 @@
 package pers.jiahao.leetcode.math;
 
 public class NO421MaximumXORofTwoNumbersinanArray {
+	public int findMaximumXOR(int[] nums) {
+	       
+        int max = Integer.MIN_VALUE;
+        for(int i = 0;i<nums.length;i++){
+            for(int j = i;j<nums.length;j++){
+                max = Math.max(nums[i]^nums[j],max);
+            }
+        }
+        return max;
+        
+    }
+	
 
 }
