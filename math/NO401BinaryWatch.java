849ec0501c751f5ffbe502e148bef927c7ba1fa6
Jiahao <JiahaoZhang2018@outlook.com>
2018-07-25 17:44:20 +0800

    Math

---
+++ math/NO401BinaryWatch.java
@@ -0,0 +1,20 @@
+package pers.jiahao.leetcode.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NO401BinaryWatch {
+	 public List<String> readBinaryWatch(int num) {
+	        List<String> result = new ArrayList<>();
+	        for(int i = 0;i<12;i++){
+	            for(int j = 0;j<=59;j++){
+	                if(Integer.bitCount(i)+Integer.bitCount(j)==num){
+	                    result.add(String.format("%d:%02d",i,j));
+	                }
+	            }
+	        }
+	        return result;
+	        
+	    }
+
+}
