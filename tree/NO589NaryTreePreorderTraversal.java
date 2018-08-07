1e8cbf30103e216aff3e639b9f70fac963bdaed8
Jiahao <JiahaoZhang2018@outlook.com>
2018-08-07 23:07:15 +0800

    Tree

--- tree/NO589NaryTreePreorderTraversal.java
+++ tree/NO589NaryTreePreorderTraversal.java
@@ -1,9 +1,35 @@
 package pers.jiahao.leetcode.tree;
-/** 
-* @author Jiahao Zhang
-* @version ：Aug 6, 2018 10:57:53 PM 
-* 类说明 
-*/
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NO589NaryTreePreorderTraversal {
+	class Node {
+	    public int val;
+	    public List<Node> children;
+
+	    public Node() {}
+
+	    public Node(int _val,List<Node> _children) {
+	        val = _val;
+	        children = _children;
+	    }
+	};
+	class Solution {
+	    public List<Integer> preorder(Node root) {
+	        List<Integer> result = new ArrayList<>();
+	        helper(result,root);
+	        return result;
+	    }
+	    private void helper(List<Integer> list,Node root){
+	        if(root==null){
+	            return;
+	        }
+	        list.add(root.val);
+	        for(int i = 0;i<root.children.size();i++){
+	            helper(list,root.children.get(i));
+	        }
+	    }
+	}
 
 }
